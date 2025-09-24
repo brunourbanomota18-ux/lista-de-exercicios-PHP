39274c8b4c12ed9dbebf2ed11d88c56b10a3e6cd
brunourbanomota18-ux <brunourbanomota18@gmail.com>
2025-09-24 17:32:18 -0300

    Update index.php

--- questoes/02/index.php
+++ questoes/02/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8"> <!-- Corrigido aqui -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
     <link rel="stylesheet" href="./../../styles.css">
@@ -11,15 +11,25 @@
 <body>
 
     <header>
-        <h2>Questão 02: Metros para centimetros</h2>
+        <h2>Questão 02: Metros para Centímetros</h2>
     </header>
 
     <main>
+        <!-- implementação da solução -->
+        <form method="post">
+            <label for="metros">Digite o valor em metros:</label>
+            <input type="number" name="metros" id="metros" step="0.01" required>
+            <button type="submit">Converter</button>
+        </form>
 
-    <!-- implementação da solução -->
-     
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $metros = $_POST["metros"];
+            $centimetros = $metros * 100;
+            echo "<p><strong>$metros</strong> metro(s) equivalem a <strong>$centimetros</strong> centímetros.</p>";
+        }
+        ?>
     </main>
 </body>
-
 
 </html>
